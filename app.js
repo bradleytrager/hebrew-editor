08eb3b2a914e32d3dfc3734df173504681a43d4d
Bradley Trager <bradley.trager@contractor.cengage.com>
2014-10-29 16:24:12 -0400

    add redis and document by hash tag

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var server = connect(
       connect.logger(),
       connect.static(__dirname + '/public')
     );
-var options = {db: {type: 'none'}}; // See docs for options. {type: 'redis'} to enable persistance.
+
+var options = {db: {type: 'redis'}}; // See docs for options. {type: 'redis'} to enable persistance.
 
 // Attach the sharejs REST and Socket.io interfaces to the server
 sharejs.attach(server, options);
