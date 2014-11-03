c756354118f94fc71c74b3ce5e6519d3608a90c4
Bradley Trager <bradleytrager@Bradleys-MacBook-Pro.local>
2014-11-02 22:16:08 -0500

    added 2nd editor

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 var connect = require('connect'),
-    sharejs = require('share').server;
+	sharejs = require('share').server;
 
 var server = connect(
-      connect.logger(),
-      connect.static(__dirname + '/public')
-    );
+	connect.logger(),
+	connect.static(__dirname + '/public')
+);
 
-var options = {db: {type: 'redis'}}; // See docs for options. {type: 'redis'} to enable persistance.
+var options = {
+	db: {
+		type: 'pg',
+		host: 'localhost',
+		user: 'root',
+		password: 'root',
+		charset: 'utf8_general_ci'
+	}
+}; // See docs for options. {type: 'redis'} to enable persistance.
 
 // Attach the sharejs REST and Socket.io interfaces to the server
 sharejs.attach(server, options);
 
-server.listen(8000, function(){
-    console.log('Server running at http://127.0.0.1:8000/');
+server.listen(8000, function() {
+	console.log('Server running at http://127.0.0.1:8000/');
 });
